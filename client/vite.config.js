a2c401e28a9b1ae9a6b1d47175938cf1e8c96966
Cedric <128242507+Cedrix-code@users.noreply.github.com>
2025-02-02 02:39:53 +0800

    vite config file update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -9,7 +9,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': '/src'
-    }
+      "@": "/src",
+    },
   },
-})
+});
