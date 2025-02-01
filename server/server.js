85409c72949a717fd850fa524e5d7010d8da8048
Cedric <128242507+Cedrix-code@users.noreply.github.com>
2025-02-02 07:43:06 +0800

    cors patch 06

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import gameRoutes from './routes/router';
+import gameRoutes from './routes/router.js';
 
 dotenv.config();
 
