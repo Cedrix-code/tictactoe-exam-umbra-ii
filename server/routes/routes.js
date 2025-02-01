df463b84a10151103b694834e904eaf2e2cf9f54
Cedric <128242507+Cedrix-code@users.noreply.github.com>
2025-02-02 03:20:58 +0800

    route mispelled

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import routes from './routes/route.js';
+import routes from './routes/routes.js';
 import { startGame, recordRound, endGame, getGames } from '../controllers/game.controller.js';
 import User from '../models/user.model.js';
 
