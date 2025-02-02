7f104a7dd13f0e92f3df7a0753e66b02fc11df34
Cedric <128242507+Cedrix-code@users.noreply.github.com>
2025-02-03 07:59:07 +0800

    server 02

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import gameRoutes from './routes/game.route.js';
+import gameRoutes from './routes/router.js';
 
 dotenv.config();
 
